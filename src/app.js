67b29aaf555372dbf69f8febab8a15f48fe3f815
song_woojin <ujins8201@gmail.com>
2023-12-30 15:15:22 +0900

    test

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
-import express from 'express';
-import router from './routes/index.js';
+import express from "express";
+import router from "./routes/index.js";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use('/', router);
+app.use("/", router);
 
 app.listen(port, () => {
-  console.log(port, '포트로 서버가 열렸어요!');
+  console.log(port, "포트로 서버가 열렸어요!");
+  console.log(process.env.DATABASE_URL);
 });
